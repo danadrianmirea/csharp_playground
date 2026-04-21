d79046e22910b2c0aa099c5c69b0a9b7928e82d9
Dan Adrian Mirea <damirea84@gmail.com>
2026-04-21 17:06:36 +0300

    add HelloMauiApp

---
+++ Graphical_MAUI/AppShell.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Maui.ApplicationModel;
+using Microsoft.Maui.Controls;
+
+namespace HelloMauiApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnFileExitClicked(object? sender, EventArgs e)
+	{
+		Application.Current?.Quit();
+	}
+
+	private async void OnHelpAboutClicked(object? sender, EventArgs e)
+	{
+		await DisplayAlertAsync("About Hello MAUI App", 
+			"This is a simple .NET MAUI Hello World application with menus.\n\n" +
+			"Created as a demonstration of MAUI development.", 
+			"OK");
+	}
+}
