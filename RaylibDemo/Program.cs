a9a1b0386516c8fa59134ef6e236b1dd920f4e18
Dan Adrian Mirea <damirea84@gmail.com>
2026-04-26 14:56:37 +0300

    add raylib 3d cube demo

---
+++ RaylibDemo/Program.cs
@@ -0,0 +1,40 @@
+using Raylib_cs;
+using System.Numerics;
+
+const int screenWidth = 800;
+const int screenHeight = 600;
+
+Raylib.InitWindow(screenWidth, screenHeight, "Raylib C# - 3D Cube Demo");
+
+Camera3D camera = new Camera3D();
+camera.Position = new Vector3(10.0f, 10.0f, 10.0f);
+camera.Target = new Vector3(0.0f, 0.0f, 0.0f);
+camera.Up = new Vector3(0.0f, 1.0f, 0.0f);
+camera.FovY = 45.0f;
+camera.Projection = CameraProjection.Perspective;
+
+Raylib.SetTargetFPS(60);
+
+while (!Raylib.WindowShouldClose())
+{
+    Raylib.UpdateCamera(ref camera, CameraMode.Orbital);
+
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.White);
+
+    Raylib.BeginMode3D(camera);
+
+    Raylib.DrawCube(new Vector3(0.0f, 0.0f, 0.0f), 2.0f, 2.0f, 2.0f, Color.Red);
+    Raylib.DrawCubeWires(new Vector3(0.0f, 0.0f, 0.0f), 2.0f, 2.0f, 2.0f, Color.Maroon);
+
+    Raylib.DrawGrid(10, 1.0f);
+
+    Raylib.EndMode3D();
+
+    Raylib.DrawText("Hello Raylib! 3D Cube", 10, 10, 20, Color.DarkGray);
+    Raylib.DrawFPS(10, 40);
+
+    Raylib.EndDrawing();
+}
+
+Raylib.CloseWindow();
